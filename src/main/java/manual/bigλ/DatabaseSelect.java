d04fd048f9f266ccce754bd363dfc4003aa81b68
Anonymous <anon@anon.com>
2017-11-06 17:58:26 -0800

    BigLambda benchmark set
    Includes original code, hand translated Spark code and automatically translated Spark code

---
+++ src/main/java/manual/bigλ/DatabaseSelect.java
@@ -0,0 +1,17 @@
+package manual.bigλ;
+
+import org.apache.spark.api.java.JavaRDD;
+
+import java.util.List;
+
+public class DatabaseSelect {
+
+	class Record {
+		public List<String> columns;
+	}
+
+	public List<Record> select(JavaRDD<Record> table, String key){
+		final String pred = key;
+		return table.filter(r -> r.columns.get(0).equals(pred)).collect();
+	}
+}
